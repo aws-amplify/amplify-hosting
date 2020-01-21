2ab8b2c9b0b25e350347a98f7d45a01d07302bd8
Joyce Hao <haoyujie@amazon.com>
2020-01-21 11:43:46 -0800

    Updating push script

--- scripts/amplifyPush.sh
+++ scripts/amplifyPush.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -e
 IFS='|'
+
 help_output () {
     echo "usage: amplify-push <--environment|-e <name>> <--simple|-s>"
     echo "  --environment  The name of the Amplify environment to use"
@@ -14,18 +15,18 @@ init_env () {
     PROVIDERS=$3
     CODEGEN=$4
     AWSCONFIG=$5
-    CATEGORIES=$6
+
     echo "# Start initializing Amplify environment: ${ENV}"
     if [[ -z ${STACKINFO} ]];
     then
         echo "# Initializing new Amplify environment: ${ENV} (amplify init)"
-        amplify init --amplify ${AMPLIFY} --providers ${PROVIDERS} --codegen ${CODEGEN} --categories ${CATEGORIES} --yes;
+        amplify init --amplify ${AMPLIFY} --providers ${PROVIDERS} --codegen ${CODEGEN} --yes;
         echo "# Environment ${ENV} details:"
         amplify env get --name ${ENV}
     else
         echo "STACKINFO="${STACKINFO}
         echo "# Importing Amplify environment: ${ENV} (amplify env import)"
-        amplify env import --name ${ENV} --config "${STACKINFO}" --awsInfo ${AWSCONFIG} --categories ${CATEGORIES} --yes;
+        amplify env import --name ${ENV} --config "${STACKINFO}" --awsInfo ${AWSCONFIG} --yes;
         echo "# Initializing existing Amplify environment: ${ENV} (amplify init)"
         amplify init --amplify ${AMPLIFY} --providers ${PROVIDERS} --codegen ${CODEGEN} --yes;
         echo "# Environment ${ENV} details:"
@@ -38,28 +39,27 @@ ENV=""
 IS_SIMPLE=false
 POSITIONAL=()
 while [[ $# -gt 0 ]]
-do
+    do
     key="$1"
     case ${key} in
         -e|--environment)
-            ENV=$2
-            shift
-            ;;
+        ENV=$2
+        shift
+        ;;
         -r|--region)
-            REGION=$2
-            shift
-            ;;
+        REGION=$2
+        shift
+        ;;
         -s|--simple)
-            IS_SIMPLE=true
-            shift
-            ;;
+        IS_SIMPLE=true
+        shift
+        ;;
         *)
-            POSITIONAL+=("$1")
-            shift
-            ;;
+        POSITIONAL+=("$1")
+        shift
+        ;;
     esac
 done
-
 set -- "${POSITIONAL[@]}"
 
 # if no provided environment name, use default env variable, then user override
@@ -67,10 +67,14 @@ if [[ ${ENV} = "" ]];
 then
     ENV=${AWS_BRANCH}
 fi
+
 if [[ ${USER_BRANCH} != "" ]];
 then
     ENV=${USER_BRANCH}
 fi
+
+# strip slashes, limit to 10 chars
+ENV=$(echo ${ENV} | sed 's;\\;;g' | sed 's;\/;;g' | cut -c -10)
 
 # Check valid environment name
 if [[ -z ${ENV} || "${ENV}" =~ [^a-zA-Z0-9\-]+ ]] ; then help_output ; fi
@@ -78,29 +82,19 @@ if [[ -z ${ENV} || "${ENV}" =~ [^a-zA-Z0-9\-]+ ]] ; then help_output ; fi
 AWSCONFIG="{\
 \"configLevel\":\"project\",\
 \"useProfile\":true,\
-\"profileName\":\"default\"\
+\"profileName\":\"default\",\
+\"AmplifyAppId\":\"${AWS_APP_ID}\"\
 }"
-
 AMPLIFY="{\
-\"envName\":\"${ENV}\"\
+\"envName\":\"${ENV}\",\
+\"appId\":\"${AWS_APP_ID}\"\
 }"
-
 PROVIDERS="{\
 \"awscloudformation\":${AWSCONFIG}\
 }"
-
 CODEGEN="{\
 \"generateCode\":false,\
 \"generateDocs\":false\
-}"
-
-AUTHCONFIG="{\
-\"facebookAppIdUserPool\":\"XXXXXXXXXXXXXX\",\
-\"facebookAppSecretUserPool\":\"XXXXXXXXXXXXXX\"\
-}"
-
-CATEGORIES="{\
-\"auth\":$AUTHCONFIG\
 }"
 
 # Handle old or new config file based on simple flag
@@ -108,12 +102,12 @@ if [[ ${IS_SIMPLE} ]];
 then
     echo "# Getting Amplify CLI Cloud-Formation stack info from environment cache"
     export STACKINFO="$(envCache --get stackInfo)"
-    init_env ${ENV} ${AMPLIFY} ${PROVIDERS} ${CODEGEN} ${AWSCONFIG} ${CATEGORIES}
+    init_env ${ENV} ${AMPLIFY} ${PROVIDERS} ${CODEGEN} ${AWSCONFIG}
     echo "# Store Amplify CLI Cloud-Formation stack info in environment cache"
     STACKINFO="$(amplify env get --json --name ${ENV})"
     envCache --set stackInfo ${STACKINFO}
     echo "STACKINFO="${STACKINFO}
 else
     # old config file, above steps performed outside of this script
-    init_env ${ENV} ${AMPLIFY} ${PROVIDERS} ${CODEGEN} ${AWSCONFIG} ${CATEGORIES}
+    init_env ${ENV} ${AMPLIFY} ${PROVIDERS} ${CODEGEN} ${AWSCONFIG}
 fi
